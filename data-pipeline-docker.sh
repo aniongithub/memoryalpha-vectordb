7a331c459ad21dd30a941e8b7c8f80d1ecdbfaa5
aniongithub <ani@anionline.me>
2025-08-04 20:12:49 +0000

    feat: enhance docker run command to conditionally use interactive mode

--- data-pipeline-docker.sh
+++ data-pipeline-docker.sh
@@ -8,4 +8,12 @@ pushd "$SCRIPT_DIR"
 trap popd EXIT
 
 docker build -t memoryalpha-pipeline -f pipeline/pipeline.Dockerfile .
-docker run --rm -it -v "$SCRIPT_DIR/data":/data memoryalpha-pipeline
+
+# Use -it only if running in a TTY
+if [ -t 1 ]; then
+  DOCKER_RUN_FLAGS="-it"
+else
+  DOCKER_RUN_FLAGS=""
+fi
+
+docker run --rm $DOCKER_RUN_FLAGS -v "$SCRIPT_DIR/data":/data memoryalpha-pipeline
